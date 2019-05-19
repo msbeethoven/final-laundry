8e3cd3dbda42e21da73df807a2473b7d274f2b81
core <seorisachs@gmail.com>
2019-05-19 17:30:25 -0400

    Client side and connected both sides

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const port = 3000;
+const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res) {
-  res.send('One More Time')
+// app.get('/', function(req, res) {
+//   res.send('Hello world')
+// })
+
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+}); 
+
+app.use ( function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Interal issues')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
